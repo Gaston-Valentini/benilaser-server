ea7e529f082da910043044177c58ec25e02aac23
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-04-02 16:25:23 +0200

    Add: Server initialized and environment variables configured.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
+
+try {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT}`);
+    });
+} catch (error) {
+    console.log(error);
+}
